a18833d14b696c791dd3f467aff7f702fde9adcb
Darrr1 <96322679+Darrr1@users.noreply.github.com>
2022-02-11 01:33:22 +0100

    Update index.js

--- src/WinXP/index.js
+++ src/WinXP/index.js
@@ -352,7 +352,7 @@ const Container = styled.div`
   height: 100%;
   overflow: hidden;
   position: relative;
-  background: url(https://i.imgur.com/Qi0z2pZ.jpg) no-repeat center center fixed;
+  background: url(https://i.imgur.com/bd7HqLi.jpg) no-repeat center center fixed;
   background-size: cover;
   animation: ${({ state }) => animation[state]} 5s forwards;
   *:not(input):not(textarea) {
