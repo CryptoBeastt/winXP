3c2867dfecda01735c8d7f40241d15d6014e7fcf
Darrr1 <96322679+Darrr1@users.noreply.github.com>
2022-02-12 21:01:46 +0100

    Update index.js

--- src/WinXP/apps/Notepad/index.js
+++ src/WinXP/apps/Notepad/index.js
@@ -1,87 +1,40 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 
-import { WindowDropDowns } from 'components';
-import dropDownData from './dropDownData';
+// add child div to capture mouse event when not focused
 
-export default function Notepad({ onClose }) {
-  const [docText, setDocText] = useState('');
-  const [wordWrap, setWordWrap] = useState(false);
-
-  function onClickOptionItem(item) {
-    switch (item) {
-      case 'Exit':
-        onClose();
-        break;
-      case 'Word Wrap':
-        setWordWrap(!wordWrap);
-        break;
-      case 'Time/Date':
-        const date = new Date();
-        setDocText(
-          `${docText}${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
-        );
-        break;
-      default:
-    }
-  }
-  function onTextAreaKeyDown(e) {
-    // handle tabs in text area
-    if (e.which === 9) {
-      e.preventDefault();
-      e.persist();
-      var start = e.target.selectionStart;
-      var end = e.target.selectionEnd;
-      setDocText(`${docText.substring(0, start)}\t${docText.substring(end)}`);
-
-      // asynchronously update textarea selection to include tab
-      // workaround due to https://github.com/facebook/react/issues/14174
-      requestAnimationFrame(() => {
-        e.target.selectionStart = start + 1;
-        e.target.selectionEnd = start + 1;
-      });
-    }
-  }
-
+function Paint({ onClose, isFocus }) {
   return (
-    <Div>
-      <section className="np__toolbar">
-        <WindowDropDowns items={dropDownData} onClickItem={onClickOptionItem} />
-      </section>
-      <StyledTextarea
-        wordWrap={wordWrap}
-        value={docText}
-        onChange={e => setDocText(e.target.value)}
-        onKeyDown={onTextAreaKeyDown}
-        spellCheck={false}
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+      }}
+    >
+      <iframe
+        src="https://www.bnbuilder.com/"
+        frameBorder="0"
+        title="paint"
+        style={{
+          display: 'block',
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'rgb(192,192,192)',
+        }}
       />
-    </Div>
+      {!isFocus && (
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+          }}
+        />
+      )}
+    </div>
   );
 }
 
-const Div = styled.div`
-  height: 100%;
-  background: linear-gradient(to right, #edede5 0%, #ede8cd 100%);
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  .np__toolbar {
-    position: relative;
-    height: 21px;
-    flex-shrink: 0;
-    border-bottom: 1px solid white;
-  }
-`;
-
-const StyledTextarea = styled.textarea`
-  flex: auto;
-  outline: none;
-  font-family: 'Lucida Console', monospace;
-  font-size: 13px;
-  line-height: 14px;
-  resize: none;
-  padding: 2px;
-  ${props => (props.wordWrap ? '' : 'white-space: nowrap; overflow-x: scroll;')}
-  overflow-y: scroll;
-  border: 1px solid #96abff;
-`;
+export default Paint;
