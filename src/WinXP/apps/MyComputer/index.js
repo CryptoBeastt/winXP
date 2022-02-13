f18b3a9d4435beaba71a3b09f7115a523ea2b510
Darrr1 <96322679+Darrr1@users.noreply.github.com>
2022-02-13 22:40:49 +0100

    Update index.js

--- src/WinXP/apps/MyComputer/index.js
+++ src/WinXP/apps/MyComputer/index.js
@@ -325,7 +325,7 @@ function MyComputer({ onClose }) {
                 >
                   <img
                     className="com__content__right__card__img"
-                    src={disk}
+                    src={logo}
                     alt="control"
                   />
                   <div className="com__content__right__card__text">test1</div>
@@ -408,42 +408,6 @@ function MyComputer({ onClose }) {
                     Telegram
                   </div>
                 </ga.OutboundLink>
-                import React from 'react';
-    <div
-      style={{
-        width: '100%',
-        height: '100%',
-        position: 'relative',
-      }}
-    >
-      <iframe
-        src="https://player.vimeo.com/video/148751763"
-        frameBorder="0"
-        title="paint"
-        style={{
-          display: 'block',
-          width: '100%',
-          height: '100%',
-          backgroundColor: 'rgb(192,192,192)',
-        }}
-      />
-      {!isFocus && (
-        <div
-          style={{
-            width: '100%',
-            height: '100%',
-            position: 'absolute',
-            left: 0,
-            top: 0,
-          }}
-        />
-      )}
-    </div>
-  );
-}
-
-export default Paint;
-
               </div>
             </div>
           </div>
