565a430e7306fd74ec78c93b1207e9bcc4934db3
Darrr1 <96322679+Darrr1@users.noreply.github.com>
2022-02-13 01:19:30 +0100

    Update index.js

--- src/WinXP/apps/MyComputer/index.js
+++ src/WinXP/apps/MyComputer/index.js
@@ -390,6 +390,46 @@ function MyComputer({ onClose }) {
     </Div>
   );
 }
+import React from 'react';
+
+// add child div to capture mouse event when not focused
+
+function Paint({ onClose, isFocus }) {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+      }}
+    >
+      <iframe
+        src="https://player.vimeo.com/video/148751763"
+        frameBorder="0"
+        title="paint"
+        style={{
+          display: 'block',
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'rgb(192,192,192)',
+        }}
+      />
+      {!isFocus && (
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Paint;
 
 const Div = styled.div`
   height: 100%;
