facc9cc9b3cf892edd0f3eb8200db6babde6ca83
Darrr1 <96322679+Darrr1@users.noreply.github.com>
2022-02-13 01:10:35 +0100

    Update index.js

--- src/WinXP/apps/Paint/index.js
+++ src/WinXP/apps/Paint/index.js
@@ -12,7 +12,7 @@ function Paint({ onClose, isFocus }) {
       }}
     >
       <iframe
-        src="https://www.bnbuilder.com/"
+        src="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
         frameBorder="0"
         title="paint"
         style={{
